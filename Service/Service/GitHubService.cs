9a1715374a4035fd8ed405b5dfbd14fca0019032
ChaniDonat <chani72901@gmail.com>
2024-02-15 11:59:51 +0200

    create project GitUb.API

---
+++ Service/Service/GitHubService.cs
@@ -0,0 +1,8 @@
+﻿namespace Service
+{
+    public class GitHubService: IGitHubService
+    {
+        string TOKENFORCVSITE = "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";
+
+    }
+}
